ae4e3013331eb1b614e8e5f0a1404f42120aceff
rafapaz <rafapaz@gmail.com>
2019-12-29 17:03:41 -0300

    Home page

--- django/poker/main/urls.py
+++ django/poker/main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='main_index'),    
+    path('', views.index, name='main_index'),
+    path('signup/', views.signup, name='main_signup'),
+    path('signin/', views.signin, name='main_signin'),
+    path('contact/', views.contact, name='main_contact'),
 ]
--- django/poker/main/views.py
+++ django/poker/main/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main/index.html')
+
+def signup(request):
+    return render(request, 'main/index.html')
+
+def signin(request):
+    return render(request, 'main/index.html')
+
+def contact(request):
+    return render(request, 'main/index.html')
